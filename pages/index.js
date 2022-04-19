a672f65b513f1fedeb5d8330e331d99dbd766ddf
Kiera Carman <kiera.carman@gmail.com>
2022-04-19 16:06:39 -0400

    Update data source

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import useSWR from 'swr'
+import { SpinnerDotted } from 'spinners-react'
 
 import fetcher from '../lib/fetcher'
 import styles from '../styles/Home.module.css'
@@ -12,20 +13,22 @@ export default function Home() {
       <Head>
         <title>Ithaca Covid Status</title>
       </Head>
-      <div className={styles.data}>
-        <h1>Community Transmission</h1>
-        <span className={styles.transmission}>
-          {data ? data[6].community_transmission : ''}
-        </span>
-        <h2>New Cases</h2>
-        <span className={styles.new}>
-          {data ? data[6].new_cases : ''}
-        </span>
-        <h2>Total Cases</h2>
-        <span className={styles.total}>
-          {data ? data[6].total_cases : ''}
-        </span>
-      </div>
+      {data ? (
+        <div className={styles.data}>
+          <h1>Community Transmission</h1>
+          <span className={styles.transmission}>
+            {data.community_transmission}
+          </span>
+          <h2>New Cases</h2>
+          <span className={styles.new}>
+            {data.new_cases}
+          </span>
+          <h2>Total Cases</h2>
+          <span className={styles.total}>
+            {data.total_cases}
+          </span>
+        </div>
+      ) : <SpinnerDotted size={172} color='#000000' />}
     </div>
   )
 }
